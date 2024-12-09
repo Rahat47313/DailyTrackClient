44bb17658165fa168c9a15869ef906895e1707c5
Rahat47313 <rahat47313@gmail.com>
2024-12-09 10:28:28 +0600

    modified Dashboard

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,23 +1,29 @@
 import axios from "axios";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import { NavLink } from "react-router-dom";
+import { setNavAndSideVisibility } from "../redux/navAndSide/navAndSideSlice";
 
 // import { useState } from "react";
 
 export default function Dashboard() {
+  const dispatch = useDispatch();
   const apiCall = () => {
     axios.get("http://localhost:3000").then((data) => {
       console.log(data);
     });
   };
+
+  useEffect(()=> {
+    dispatch(setNavAndSideVisibility(false))
+  })
   return (
     <>
-      <div className="text-gray-900 dark:text-white p-4 md:ml-64 mt-[60px]">
-      <button
-            onClick={apiCall}
-            className="fixed top-1.5 left-1/2 z-[100] text-white bg-gray-950 rounded-md py-3 px-5"
-          >
-            <NavLink to="/login">Call API & Login</NavLink>
-          </button>
+      <div className="flex flex-col justify-center items-center gap-10 h-screen">
+        <p className="text-3xl">Just a temporary landing page</p>
+        <button onClick={apiCall} className="bg-gray-950 rounded-md py-3 px-5">
+          <NavLink to="/login" onClick={()=> dispatch(setNavAndSideVisibility(true))}>Call API & Login</NavLink>
+        </button>
       </div>
     </>
   );
